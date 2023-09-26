4e2cfed505e48b0e4ee1eb1c767940fed3b690ea
Alexandre Machado <amch.smd@gmail.com>
2023-09-26 17:50:29 -0300

    refactor: add type annotations to store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,26 +1,38 @@
 import { create } from "zustand";
 
+import { VolumeData } from "../types";
+
 interface BookStore {
-  volumes: unknown[];
-  currentVolume: unknown;
+  volumes: VolumeData[];
+  suggestions: VolumeData[];
+  currentVolume: VolumeData;
   currentPage: number;
   filters: unknown;
-  setVolumes(volumes: unknown[]): void;
-  setCurrentVolume(volume: unknown): void;
+
+  setVolumes(volumes: VolumeData[]): void;
+  setSuggestions(suggestions: VolumeData[]): void;
+  setCurrentVolume(volume: VolumeData): void;
   setCurrentPage(page: number): void;
   setFilters(filters: unknown): void;
 }
 
 const useStore = create<BookStore>()((set) => ({
   volumes: [],
-  currentVolume: {},
+  suggestions: [],
+  currentVolume: <VolumeData>{},
   currentPage: 1,
   filters: {},
+
   setVolumes: (volumes) => set((state) => ({ ...state, volumes })),
+
+  setSuggestions: (suggestions) => set((state) => ({ ...state, suggestions })),
+
   setCurrentVolume: (currentVolume) =>
     set((state) => ({ ...state, currentVolume })),
+
   setCurrentPage: (page: number) =>
     set((state) => ({ ...state, currentPage: page })),
+
   setFilters: (filters) => set((state) => ({ ...state, filters })),
 }));
 
