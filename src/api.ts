59c8db23027cd869cc3f2cec35f7b80982051450
Alexandre Machado <amch.smd@gmail.com>
2023-09-26 19:07:30 -0300

    refactor: add separated api method for shelves

--- src/api.ts
+++ src/api.ts
@@ -5,17 +5,12 @@ const ROOT_URL = "https://www.googleapis.com/books/v1";
 export const getVolumes = async (
   query: string,
   startIndex: number = 0,
-  maxResults: number = 10,
-  subjects: string[] = ["action"]
+  maxResults: number = 10
 ): Promise<VolumeData[] | undefined> => {
-  const queryString = subjects.length
-    ? `${query}+subject:${subjects.join("+")}`
-    : query;
-
   const params = new URLSearchParams({
     startIndex: startIndex.toString(),
     maxResults: maxResults.toString(),
-    q: queryString,
+    q: query,
   });
 
   const response = await fetch(`${ROOT_URL}/volumes?${params.toString()}`);
@@ -32,6 +27,21 @@ export const getVolumeDetails = async (
   id: number
 ): Promise<VolumeData | undefined> => {
   const response = await fetch(`${ROOT_URL}/volumes/${id}`);
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    return Promise.reject(new Error(errorMessage));
+  }
+
+  return await response.json();
+};
+
+export const getSubjects = async (subjects: string[]) => {
+  const params = new URLSearchParams({
+    q: `subject:${subjects.join("+")}`,
+  });
+
+  const response = await fetch(`${ROOT_URL}/volumes?q=${params.toString()}`);
 
   if (!response.ok) {
     const errorMessage = await response.text();
--- src/components/shelves/index.tsx
+++ src/components/shelves/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import {
   Container,
   Content,
@@ -11,29 +11,41 @@ import { settings } from "../carousel/constants";
 import Carousel from "../carousel";
 import { books } from "./constants";
 
+import { getSubjects } from "../../api";
+
+const CATEGORIES = ["adventure", "fiction", "action"];
+
 const Shelves: React.FC = () => {
+  const handleSearch = useCallback(async () => {
+    return await getSubjects(CATEGORIES);
+  }, []);
+
+  useEffect(() => {
+    handleSearch();
+  }, []);
+
   return (
     <Container>
       <Shelf>
-      {books.map((shelf) => {
-        return (
-          <ContentList>
-            <TitleCategory key={shelf.id}>{shelf.shelfCategory}</TitleCategory>
-            <Content>
-
+        {books.map((shelf) => {
+          return (
+            <ContentList>
+              <TitleCategory key={shelf.id}>
+                {shelf.shelfCategory}
+              </TitleCategory>
+              <Content>
                 <Carousel settings={settings} spaceBetweenItems="16px">
-                {shelf.booksShelf.map((book) => (
+                  {shelf.booksShelf.map((book) => (
                     <ContentItem key={book.title}>
-                        <img src={book.urlImage} alt={book.slug} />
+                      <img src={book.urlImage} alt={book.slug} />
                     </ContentItem>
-                ))}
+                  ))}
                 </Carousel>
-            </Content>
-
-          </ContentList>
-        );
-      })}
-    </Shelf>
+              </Content>
+            </ContentList>
+          );
+        })}
+      </Shelf>
     </Container>
   );
 };
