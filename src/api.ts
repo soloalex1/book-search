788f4192d1e541ea9af887c1ac7a3375ddfe2831
Alexandre Machado <amch.smd@gmail.com>
2023-09-27 07:37:40 -0300

    refactor: return volumes directly on API

--- src/api.ts
+++ src/api.ts
@@ -2,11 +2,17 @@ import { VolumeData } from "./types";
 
 const ROOT_URL = "https://www.googleapis.com/books/v1";
 
+type JSONResponse = {
+  kind: string;
+  totalItems: number;
+  items: VolumeData[];
+};
+
 export const getVolumes = async (
   query: string,
   startIndex: number = 0,
   maxResults: number = 10
-): Promise<VolumeData[] | undefined> => {
+): Promise<VolumeData[]> => {
   const params = new URLSearchParams({
     startIndex: startIndex.toString(),
     maxResults: maxResults.toString(),
@@ -20,20 +26,9 @@ export const getVolumes = async (
     return Promise.reject(new Error(errorMessage));
   }
 
-  return await response.json();
-};
+  const { items }: JSONResponse = await response.json();
 
-export const getVolumeDetails = async (
-  id: number
-): Promise<VolumeData | undefined> => {
-  const response = await fetch(`${ROOT_URL}/volumes/${id}`);
-
-  if (!response.ok) {
-    const errorMessage = await response.text();
-    return Promise.reject(new Error(errorMessage));
-  }
-
-  return await response.json();
+  return items;
 };
 
 export const getSubjects = async (subjects: string[]) => {
@@ -53,4 +48,17 @@ export const getSubjects = async (subjects: string[]) => {
   });
 
   return Promise.all(allResponses);
+};
+
+export const getVolumeDetails = async (
+  id: number
+): Promise<VolumeData | undefined> => {
+  const response = await fetch(`${ROOT_URL}/volumes/${id}`);
+
+  if (!response.ok) {
+    const errorMessage = await response.text();
+    return Promise.reject(new Error(errorMessage));
+  }
+
+  return await response.json();
 };
