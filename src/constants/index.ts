46043ca989eb1eaf2d8e246704e9290e96349743
Alexandre Machado <amch.smd@gmail.com>
2023-09-29 17:50:26 -0300

    style: add breakpoint constant values

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,4 +1,12 @@
-export const footerAllrightsReserved = 'Copyright © 2023 Árvore. Todos os direitos reservados.'
-export const privacyAndPolicy = 'Política e privacidade'
-export const termsAndUsage = 'Termos de uso'
-export const helpLink = 'Ajuda'
+export const footerAllrightsReserved =
+  "Copyright © 2023 Árvore. Todos os direitos reservados.";
+export const privacyAndPolicy = "Política e privacidade";
+export const termsAndUsage = "Termos de uso";
+export const helpLink = "Ajuda";
+
+export const breakpoints = {
+  tabletSM: "768px",
+  desktopMD: "992px",
+  desktopLG: "1216px",
+  desktopXL: "1408px",
+} as const;
